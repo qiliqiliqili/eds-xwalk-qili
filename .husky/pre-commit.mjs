519c09fcc16098d55b2a4205d9130e79c82a955e
qiiiliii <qilil@adobe.com>
2026-03-15 19:25:58 +0900

    fixed lint

--- .husky/pre-commit.mjs
+++ .husky/pre-commit.mjs
@@ -18,3 +18,19 @@ if (modifledPartials.length > 0) {
   console.log(output);
   await run('git add component-models.json component-definition.json component-filters.json');
 }
+
+// lint staged JS/CSS files
+const stagedJs = modifiedFiles.filter((f) => f.match(/\.(js|mjs|json)$/));
+const stagedCss = modifiedFiles.filter((f) => f.match(/\.css$/));
+
+if (stagedJs.length > 0) {
+  console.log('Running ESLint on staged files...');
+  const jsOutput = await run(`npx eslint ${stagedJs.join(' ')}`).catch((e) => { throw e; });
+  if (jsOutput) console.log(jsOutput);
+}
+
+if (stagedCss.length > 0) {
+  console.log('Running Stylelint on staged files...');
+  const cssOutput = await run(`npx stylelint ${stagedCss.join(' ')}`).catch((e) => { throw e; });
+  if (cssOutput) console.log(cssOutput);
+}
